348ef89381bb4665cd3fae5c3f32100d6db9bf78
Qichen1996 <72041979+Qichen1996@users.noreply.github.com>
2023-10-01 19:19:09 +0200

    Update config.py

--- env/config.py
+++ env/config.py
@@ -20,6 +20,6 @@ numConnModes = 3
 # delayAppWeights = [1/t/3 for t in delayBudgets]  # weighted average in the range of [0, 1]
 # dropRatioWeight = 0.5  # penalty for average drop ratio = 1%
 # delayWeight = 0.1  # penalty for average delay = delay budget
-# pcWeight = 1e-3  # PC (unit: W) penalty weight
+pcWeight = 0.4  # PC (unit: kW) penalty weight
 qosWeight = 4  # QoS reward weight
 extraQosWeight = 0.005 # QoS reward weight for done UEs
